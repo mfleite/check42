3099f18ff1faa16ba5741b28b62ae7d50ad34bd9
mfleite <manuel@freitasleite.pt>
2026-07-28 02:33:14 +0100

    Modified test.

--- C00/checks.py
+++ C00/checks.py
@@ -1,7 +1,17 @@
 import check50
 
+main_str = """
+
+
+int main()
+{
+    ft_putchar('A');
+}
+"""
+
 @check50.check()
 def ft_putchar_compiles():
     """ft_putchar compiles"""
     check50.include("test_00.c", "ex00.h")
+    check50.run(f"echo {main_str} >> test_00.c")
     check50.run("gcc -Wall -Werror -Wextra test_00.c ft_putchar.c ").exit(0)
