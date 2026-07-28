2042a6d985bcb5c13e99ae47aa67e76892b04668
mfleite <manuel@freitasleite.pt>
2026-07-28 01:47:39 +0100

    First commit. Test

---
+++ C00/checks.py
@@ -0,0 +1,7 @@
+import check50
+
+@check50.check()
+def ft_putchar_compiles():
+    """ft_putchar compiles"""
+    check50.include("test_00.c", "ex00.h")
+    check50.c.compile("ft_putchar.c", "test_00.c", "ex00.h")
---
+++ C00/test_00.c
@@ -0,0 +1,6 @@
+#include "ex00.h"
+
+int	main(int argc, char* argv[])
+{
+	ft_putchar(argv[1][0]);
+}
